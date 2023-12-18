9cc687ec3b012ea0a9a6e631721672e96f239676
turanEbrar <ebrarkckshn@gmail.com>
2023-12-19 01:39:31 +0300

    turanDeneme
    sj

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,123 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget currentContent =
+      Denemewidget1(); 
+
+  void degistir() {
+    setState(() {
+      currentContent = DenemeWidget2();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Üst'),
+              
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              currentContent, 
+            ]),
+          ),
+          SliverToBoxAdapter(
+            child: ElevatedButton(
+              onPressed: degistir,
+              child: Text('Değiştir'),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 100.0,
+              color: Colors.blue,
+              child: Center(
+                child: Text('Alt'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Denemewidget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: SizedBox(
+        height: 400,
+        child: Center(
+          child: Text('1'),
+        ),
+      ),
+    );
+  }
+}
+
+class DenemeWidget2 extends StatefulWidget {
+  @override
+  State<DenemeWidget2> createState() => _DenemeWidget2State();
+}
+
+class _DenemeWidget2State extends State<DenemeWidget2> {
+  int sayi = 0;
+  void sayiArt() {
+    setState(() {
+      sayi++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      color: Colors.orange,
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            sayiArt();
+          },
+          child: Text("DENEME Sayi bas buraya = "+sayi.toString()),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
+
+void main() {
   runApp(const MyApp());
 }
 //asa
@@ -41,3 +158,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
